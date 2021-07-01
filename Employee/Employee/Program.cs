3fb277f71093712f34350d5ab0d9c252949fed82
afreen505 <afreen.fathimaa9876@gmail.com>
2021-07-01 08:47:53 +0530

    program

---
+++ Employee/Employee/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace UC_1_EmpPresence
+{
+    class UC_1_EmpPresence
+    {
+        static void Main(string[] args)
+        {
+            int IS_FULL_Time = 1;
+            Random random = new Random();
+            int result = random.Next(0, 2);
+            if (result == IS_FULL_Time)
+                Console.WriteLine("Employee is Present");
+            else
+                Console.WriteLine("Employee is absent");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ UC_5_EmpWagesOfMonth/UC_5_EmpWagesOfMonth/UC_5_EmpWagesOfMonth.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace UC_5_EmpWagesOfMonth
+{
+    class UC_5_EmpWagesOfMonth
+    {
+
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
+        public const int NUM_OF_WORKING_DAYS = 20;
+        static void Main(string[] args)
+        {
+
+            //Variables
+            int empHrs = 0;
+            int empWage = 0;
+            int totalEmpWage = 0;
+            //Computation
+            for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
+            {
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                empWage = empHrs * EMP_RATE_PER_HOUR;
+                totalEmpWage += empWage;
+                Console.WriteLine("Emp Wage of day : " + (day + 1) + " is : " + empWage);
+            }
+            Console.WriteLine("Total Emp Wage : " + totalEmpWage);
+        }
+    }
+}
